8ace68c4b7609b90d106807b4b9ff7498f6b9e10
karthikeyansa <karthikeyansa39@gmail.com>
2020-04-29 23:55:53 +0530

    bugs

--- login/app/forms.py
+++ login/app/forms.py
@@ -4,7 +4,3 @@ class Sign(forms.Form):
 	name = forms.CharField(max_length = 20,widget = forms.TextInput(attrs = {'class' : 'form-control','placeholder':'Name'}))
 	password = forms.CharField(widget = forms.TextInput(attrs ={'class' : 'form-control','placeholder':'password'}))
 
-class Index(forms.Form):
-	name = forms.CharField(max_length = 20,
-		widget = forms.TextInput(attrs = {'class' : 'form-control','placeholder':'Name'}))
-	password = forms.CharField(widget = forms.TextInput(attrs ={'class' : 'form-control','placeholder':'password'}))
--- login/app/views.py
+++ login/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import User
-from .forms import Sign,Index
+from .forms import Sign
 
 def welcome(request,id):
 	user = get_object_or_404(User,pk = id)
@@ -11,13 +11,14 @@ def sign(request):
 	if request.method == 'POST':
 		form = Sign(request.POST)
 		if form.is_valid():
-			new = User.objects.get(name = request.POST['name'])
+			new = User.objects.filter(name__iexact = request.POST['name'])
 			if not new:
 				new = User(name = request.POST['name'],password=request.POST['password'])
 				new.save()
-				return redirect(welcome,id = new.id)
+				id = new.id
+				return redirect(welcome,id = id)
 			else:
-				return render(request,'login/sign.html',{'msg':'user already exists'})
+				return render(request,'login/sign.html',{'msg':'account exists'})		
 	else:
 		form = Sign()
 	context = {'form':form}
@@ -25,7 +26,7 @@ def sign(request):
 
 def index(request):
 	if request.method == 'POST':
-		form = Index(request.POST)
+		form = Sign(request.POST)
 		if form.is_valid():
 			name = request.POST['name']
 			password = request.POST['password']
@@ -34,6 +35,6 @@ def index(request):
 				return redirect(welcome,id = user.id)
 				
 	else:
-		form = Index()
+		form = Sign()
 	context = {'form':form}
 	return render(request,'login/index.html',context,)
