60d515924234aeabe98f1aed0ede206f395ab813
karthikeyansa <karthikeyansa39@gmail.com>
2020-06-01 12:40:38 +0530

    mysql

--- login/app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class Sign(forms.Form):
-	name = forms.CharField(max_length = 20,widget = forms.TextInput(attrs = {'class' : 'form-control','placeholder':'Name'}))
-	password = forms.CharField(widget = forms.TextInput(attrs ={'class' : 'form-control','placeholder':'password'}))
-
